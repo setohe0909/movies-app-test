0e769e4e9c239d63c5280e57f24d8471b0a7a663
sebastian tobon <setohe.09@gmail.com>
2019-12-02 10:41:09 -0500

    Changes in apiservices and html format

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Movie } from '../interfaces/movie';
+
 import { LocalStorageUtils } from '../helper/localStorage-utils';
 import { HttpClient } from '@angular/common/http';
-import { timeout } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  constructor(private http: HttpClient) {}
+  movies: any;
+
+  constructor(private http: HttpClient) {
+    this.movies = this.getAll();
+  }
+
+  // one-liner methods
+  getByName = (title: string) => this.movies.find((m: any) => m.title.toUpperCase() === title.toUpperCase()) || {};
 
   getAll = (): Movie[] => {
     let movies: Movie[] = LocalStorageUtils.getstorage('movies');
@@ -18,32 +25,24 @@ export class ApiService {
     return movies;
   };
 
-  getByName = (title: string) => {
-    const movies: Movie[] = this.getAll();
-    return movies.find((m) => m.title.toUpperCase() === title.toUpperCase()) || {};
-  };
-
   save = (movie: Movie) => {
-    const movies: Movie[] = this.getAll();
-    const indx = movies.findIndex((m) => m.title.toUpperCase() === movie.title.toUpperCase());
-    if (indx > -1) {
-      movies[indx] = movie;
-    } else {
-      movies.push(movie);
-    }
-    LocalStorageUtils.setStorage('movies', JSON.stringify(movies));
+    this.indexMovie(movie);
+
+    LocalStorageUtils.setStorage('movies', JSON.stringify(this.movies));
   };
 
-  List = () => {
-    const movies: Movie[] = this.getAll();
-    return movies;
+  indexMovie = (movie: any) => {
+    const indx = this.movies.findIndex((m: any) => m.title.toUpperCase() === movie.title.toUpperCase());
+
+    if (indx > -1) {
+      this.movies[indx] = movie;
+    } else {
+      this.movies.push(movie);
+    }
   };
 
   getTop = async () => {
-    const objTop = await this.http
-      .get('http://www.mocky.io/v2/5dc3c053300000540034757b')
-      .pipe(timeout(4000))
-      .toPromise();
+    const objTop = await this.http.get('http://www.mocky.io/v2/5dc3c053300000540034757b').toPromise();
 
     return Object.values(objTop)[0];
   };
@@ -51,9 +50,16 @@ export class ApiService {
   remove = (title: string) => {
     const movies: Movie[] = this.getAll();
     const delIndx = movies.findIndex((m) => m.title === title);
+
     if (delIndx >= 0) {
       movies.splice(delIndx, 1);
     }
+
     LocalStorageUtils.setStorage('movies', movies);
+  };
+
+  List = () => {
+    const movies: Movie[] = this.getAll();
+    return movies;
   };
 }
